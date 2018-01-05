8b98155d4e46aa4fa6a28196e4b0621793ebb2c9
sahir <sahir@punchh.com>
2018-01-05 12:08:56 +0530

    CardIOModule
    This module abstracts the CardIOPaymentViewController on iOS and the
    CardIOActivity on Android

--- App.js
+++ App.js
@@ -4,54 +4,61 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+import { CardIOModule, CardIOUtilities } from "react-native-awesome-card-io";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 export default class App extends Component<{}> {
+  componentWillMount() {
+    if (Platform.OS === "ios") {
+      CardIOUtilities.preload();
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TouchableOpacity onPress={this.scanCard.bind(this)}>
+          <Text>Scan card!</Text>
+        </TouchableOpacity>
       </View>
     );
+  }
+
+  scanCard() {
+    CardIOModule.scanCard()
+      .then(card => {
+        // the scanned card
+      })
+      .catch(() => {
+        // the user cancelled
+      });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
